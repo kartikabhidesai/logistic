3faac880cf8529e7f8b4bcd3b9fa94dc02583bb3
unknown <kartikdesai123@gmail.com>
2020-03-11 14:33:28 +0530

    respoonsive

--- resources/views/frontend/pages/aboutus.blade.php
+++ resources/views/frontend/pages/aboutus.blade.php
@@ -27,20 +27,31 @@
             <div class="col-xs-12">
                 <h2 class="text-uppercase text-center">WHO WE ARE</h2>
                 <div class="video-holder">
-                    <img src="{{ asset('public/frontend/images/img44.jpg')}}" alt="image description" class="img-responsive">
-                    <a href="http://www.youtube.com/embed/9bZkp7q19f0?autoplay=1" class="play-icon text-center round lightbox fancybox.iframe"><i class="ti-control-play"></i></a>
+                    <!--<img src="{{ asset('public/frontend/images/img44.jpg')}}" alt="image description" class="img-responsive">-->
+                    <!--<a href="http://www.youtube.com/embed/9bZkp7q19f0?autoplay=1" class="play-icon text-center round lightbox fancybox.iframe"><i class="ti-control-play"></i></a>-->
                 </div>
             </div>
         </div>
         <div class="row mar display-flex">
-            <div class="col-xs-12 col-sm-8 col-md-6">
-                <strong class="sub-title">We value our clients and have executed successful supply chain transformation projects</strong>
-                <p>Leo Global Logistics providers face a highly competitive marketplace where success depends on being cost-competitive while accommodating rapidly changing customer demands. On-boarding new customers quickly and successfully requires teamwork from all areas of your company.</p>
-                <p style="font-style: italic;">“ The difference between success and failure is being able to scale, flex and respond – with the right expertise. With the right partner in place to provide you with proven and experienced resources, you can mitigate risk, build your client base, and improve profitability. ”</p>
-            </div>
             <div class="col-xs-12 col-sm-4 col-md-6">
                 <div class="img-holder text-center"><img src="{{ asset('public/frontend/images/icon09.jpg')}}" alt="image description" class="img-responsive"></div>
             </div>
+            <div class="col-xs-12 col-sm-8 col-md-6">
+                <strong class="sub-title">We value our clients and have executed successful supply chain transformation projects</strong>
+                <p>If you are looking for specialized logistical services to ensure that your business achieves its operational goals, then LEO GLOBAL LOGISTIC is your trusted partner for business support in global markets.</p>
+                <p style="font-style: italic;">Our employees consistently deliver top quality services and logistics, and continuously evaluate and improve our customer experience. We strive for excellence by exceeding our customers’ expectations.</p>
+            </div>
+        </div>
+        <div class="row mar display-flex">
+<!--            <div class="col-xs-12 col-sm-4 col-md-6">
+                <div class="img-holder text-center"><img src="{{ asset('public/frontend/images/icon10.jpg')}}" alt="image description" class="img-responsive"></div>
+            </div>-->
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <!--<strong class="sub-title">We value our clients and have executed successful supply chain transformation projects</strong>-->
+                <p>Leo Global Logistics providers face a highly competitive marketplace where success depends on being cost-competitive while accommodating rapidly changing customer demands. On-boarding new customers quickly and successfully requires teamwork from all areas of your company.</p>
+                <p style="font-style: italic;">“ The difference between success and failure is being able to scale, flex and respond – with the right expertise. With the right partner in place to provide you with proven and experienced resources, you can mitigate risk, build your client base, and improve profitability. ”</p>
+            </div>
+            
         </div>
 <!--        <div class="row display-flex">
             <div class="col-xs-12 col-sm-4 col-md-6">
